55c34cfbaa61bc037d41a5fce466d496360c358f
MashaBarsukova <marybarskova@gmail.com>
2018-02-11 18:23:54 +0200

    change files

---
+++ js/functions.js
@@ -0,0 +1,10 @@
+const vacation = prompt ('1-taba, 2-sharm, 3-hurgada', '');
+if (vacation === '1') {
+  alert('taba');
+}else if (vacation === '2') {
+  alert('sharm');
+}else if (vacation === '3') {
+  alert('hurgada');
+}else {
+  alert('Не найдено варианта');
+}
